909be81c60eefad76034e2e2e155fd5a88226688
sooyeon0o0 <sooyeon01160306@gmail.com>
2026-04-03 23:15:01 +0900

    0402과제
    0402 과제 추가 버전(nav_button 수정)

--- example/lib/ui/pages/todo/botton_navigation/wedget/nav_button.dart
+++ example/lib/ui/pages/todo/botton_navigation/wedget/nav_button.dart
@@ -20,6 +20,22 @@ class NavButton extends StatelessWidget {
         color: selected ? Process.done.primaryColor : Colors.transparent,
         borderRadius: .circular(50),
       ),
+
+      child: Column(
+        //       MainAxisAlignment mainAxisAlignment = MainAxisAlignment.start,
+        // MainAxisSize mainAxisSize = MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: selected ? Colors.white : Colors.grey, size: 25),
+          Text(
+            text,
+            style: TextStyle(
+              color: selected ? Colors.white : Colors.grey,
+              fontSize: 12,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- example/lib/ui/pages/todo/widget/progress_card.dart
+++ example/lib/ui/pages/todo/widget/progress_card.dart
@@ -13,48 +13,41 @@ class ProgressCard extends StatelessWidget {
       ),
       child: Column(
         spacing: 15, // 요소 사이 간격 넓히기
-        crossAxisAlignment: .start,    
+        crossAxisAlignment: .start,
         children: [
           DefaultTextStyle(
-            style: TextStyle( // 텍스트 기본 스타일
+            style: TextStyle(
+              // 텍스트 기본 스타일
               color: Colors.white,
             ),
-            child: Row( // text의 가로 영역 전체 표시
+            child: Row(
+              // text의 가로 영역 전체 표시
               mainAxisAlignment: .spaceBetween,
-              
-              children: [  
+
+              children: [
                 Text(
                   '오늘의 진행률',
-                  style: TextStyle(
-                    fontWeight: .w700,
-                    fontSize: 16,
+                  style: TextStyle(fontWeight: .w700, fontSize: 16),
                 ),
-              ),
-                Text(
-                  '60%',
-                  style: TextStyle(
-                    fontWeight: .bold,
-                    fontSize: 30,
-                  ),
-                ),
+                Text('60%', style: TextStyle(fontWeight: .bold, fontSize: 30)),
               ],
             ),
-          ), 
-          // TODO: Progress표시
+          ),
+          // Progress표시
           SizedBox(
             height: 8,
             child: LinearProgressIndicator(
               value: 0.6, // 60%
-              backgroundColor: Colors.white.withValues(alpha: 0.2), //withValues -> 투명도
+              backgroundColor: Colors.white.withValues(
+                alpha: 0.2,
+              ), //withValues -> 투명도
               color: Colors.white,
               borderRadius: .circular(10),
             ),
-          ),         
+          ),
           Text(
             '10개 중 6개 완료',
-            style: TextStyle(
-              color: Colors.white.withValues(alpha: 0.6),
-            ),
+            style: TextStyle(color: Colors.white.withValues(alpha: 0.6)),
           ),
         ],
       ),
